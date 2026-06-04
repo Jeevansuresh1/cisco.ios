b88b2a9685c3e838edaa3706bdd3a703e75df2d3
Jeevan S <jes@redhat.com>
2026-06-04 21:43:51 +0530

    Update plugins/module_utils/network/ios/config/ntp_global/ntp_global.py
    Co-authored-by: ansieyes-bot-local[bot] <271158347+ansieyes-bot-local[bot]@users.noreply.github.com>

--- plugins/module_utils/network/ios/config/ntp_global/ntp_global.py
+++ plugins/module_utils/network/ios/config/ntp_global/ntp_global.py
@@ -162,7 +162,7 @@ class Ntp_global(ResourceModule):
             if k in tmp_data and k != "access_group":
                 if k in ["servers", "peers"]:
 for i in tmp_data.get
-                        pass
+pass
                             i.pop("source", None)
                 tmp_data[k] = {str(i[p_key[k]]): i for i in tmp_data[k]}
             elif tmp_data.get("access_group") and k == "access_group":
